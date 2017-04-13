f064fbc42dced265071006cabf605f321e464faa
fastily <fastily@users.noreply.github.com>
2017-04-13 00:02:01 -0700

    bug fix and refactor for SimpleUpload

--- src/main/java/enwp/tools/SimpleUpload.java
+++ src/main/java/enwp/tools/SimpleUpload.java
@@ -2,7 +2,6 @@ package enwp.tools;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.Stream;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
@@ -26,16 +25,6 @@ public class SimpleUpload
 	private static Wiki wiki;
 
 	/**
-	 * Matches filenames that may be uploaded to {@code wiki}
-	 */
-	private static String extRegex;
-
-	/**
-	 * The default file description text to use. Default value is the empty String.
-	 */
-	private static String desc;
-
-	/**
 	 * Main driver.
 	 * 
 	 * @param args Program arguments.
@@ -43,15 +32,14 @@ public class SimpleUpload
 	public static void main(String[] args)
 	{
 		CommandLine l = FCLI.gnuParse(makeOptList(), args, "SimpleUpload [-c] [-d] [-h] files");
-		desc = l.getOptionValue('d', "");
 
 		wiki = Toolbox.getFastilyClone();
 		if (l.hasOption('c'))
 			wiki = Toolbox.getCommons(wiki);
 
-		extRegex = WikiX.allowedFileExtsRegex(wiki);
-
-		Stream.of(args).filter(s -> s.matches(extRegex)).map(Paths::get).filter(Files::exists)
+		String extRegex = WikiX.allowedFileExtsRegex(wiki), desc = l.getOptionValue('d', "");
+		
+		l.getArgList().stream().filter(s -> s.matches(extRegex)).map(Paths::get).filter(Files::exists)
 				.forEach(p -> wiki.upload(p, p.getFileName().toString(), desc, ""));
 	}
 
